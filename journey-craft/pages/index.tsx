59694fc98c6ad9a136e32952c109e7f327550171
buakbuakbuak <134906477+buakbuakbuak@users.noreply.github.com>
2023-05-30 02:22:52 +0800

    Update index.tsx

--- journey-craft/pages/index.tsx
+++ journey-craft/pages/index.tsx
@@ -13,15 +13,10 @@ const HomePage: React.FC = () => {
         <Image alt="Journey Craft Logo" width={500} height={500} src={ImageLogo}/>
         <h1 className='text-5xl'>Welcome to JourneyCraft</h1>
         <p className='text-xl'>Experience personalized travel itineraries for the post-pandemic era.</p>
-        <Link className="text-lg" href = "/plan">
+        <Link className="text-lg" href = "/generate">
           <button className="border border-2 hover:border-white px-5 h-full w-full flex justify-evenly items-center hover:bg-gradient-to-r hover:from-violet-200 hover:to-emerald-200">Get Started</button>
         </Link>
         <Link className="border border-2 text-lg w-auto hover:border-white px-5 h-full w-auto flex justify-evenly items-center hover:bg-gradient-to-r hover:from-violet-200 hover:to-emerald-200" href="/login">Login</Link>
-
-      <br></br>
-      <Link href = "/generate">
-          <button>Sirui</button>
-      </Link>
       </div>
 
     </Layout>
