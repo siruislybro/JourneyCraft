5e47ee39e4cc1c2ef34d295f56665e207791c4ab
ldinghan <u.dinghan@gmail.com>
2023-05-30 01:31:49 +0800

    fix styling

--- journey-craft/pages/index.tsx
+++ journey-craft/pages/index.tsx
@@ -9,19 +9,20 @@ import ImageLogo from '../assets/JourneyCraftBoat.jpg';
 const HomePage: React.FC = () => {
   return (
     <Layout>
-      <div>
+      <div className='flex flex-col justify-center items-center'>
         <Image alt="Journey Craft Logo" width={500} height={500} src={ImageLogo}/>
-        <h1>Welcome to JourneyCraft</h1>
-        <p>Experience personalized travel itineraries for the post-pandemic era.</p>
-        <Link href = "/plan">
-          <button>Get Started</button>
+        <h1 className='text-5xl'>Welcome to JourneyCraft</h1>
+        <p className='text-xl'>Experience personalized travel itineraries for the post-pandemic era.</p>
+        <Link className="text-lg" href = "/plan">
+          <button className="border border-2 hover:border-white px-5 h-full w-full flex justify-evenly items-center hover:bg-gradient-to-r hover:from-violet-200 hover:to-emerald-200">Get Started</button>
         </Link>
-        <Link href="/login">login</Link>
-      </div>
+        <Link className="border border-2 text-lg w-auto hover:border-white px-5 h-full w-full flex justify-evenly items-center hover:bg-gradient-to-r hover:from-violet-200 hover:to-emerald-200" href="/login">Login</Link>
+
       <br></br>
       <Link href = "/generate">
           <button>Sirui</button>
       </Link>
+      </div>
 
     </Layout>
   );
