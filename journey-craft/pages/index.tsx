942581219e6f7c2bab887bcbee8a9ecb081f41cc
ldinghan <u.dinghan@gmail.com>
2023-05-30 01:27:51 +0800

    include image

--- journey-craft/pages/index.tsx
+++ journey-craft/pages/index.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import Link from 'next/link';
 import Layout from '@/components/layout';
-
+import Image from 'next/image';
+import ImageLogo from '../assets/JourneyCraftBoat.jpg';
 
 const HomePage: React.FC = () => {
   return (
     <Layout>
       <div>
+        <Image alt="Journey Craft Logo" width={500} height={500} src={ImageLogo}/>
         <h1>Welcome to JourneyCraft</h1>
         <p>Experience personalized travel itineraries for the post-pandemic era.</p>
         <Link href = "/plan">
