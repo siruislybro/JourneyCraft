da4baa19910d237083595eda54ac2922bd01434a
jinyuan0425 <pdmon123@gmail.com>
2023-05-30 08:32:44 +0800

    nav bar update
    reflects logged in state

--- journey-craft/components/navigationBar.tsx
+++ journey-craft/components/navigationBar.tsx
@@ -5,10 +5,6 @@ function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem('isLoggedIn', isLoggedIn.toString());
-  }, [isLoggedIn]);
-  
-  useEffect(() => {
     const savedIsLoggedIn = localStorage.getItem('isLoggedIn');
     if (savedIsLoggedIn) {
       setIsLoggedIn(savedIsLoggedIn === 'true');
@@ -16,25 +12,44 @@ function Navbar() {
   }, []);
 
   const handleLogout = () => {
-    // Perform logout logic here
-    // For example, clear the user session and update the logged-in state
-    localStorage.setItem("isLoggedIn", "false");
+    localStorage.setItem('isLoggedIn', 'false');
     setIsLoggedIn(false);
-    window.location.href = '/'
+    // Perform any additional logout logic here, such as clearing user session or making API requests
+    // Redirect the user to the desired page after logout
+    window.location.href = '/';
   };
-  
+
   return (
     <nav className="drop-shadow-md flex h-16 bg-gradient-to-l from-pink-400 to-emerald-500 via-indigo-400 px-2 py-[1vh] sm:px-4 fixed w-full z-20 top-0 left-0">
       <div className="container flex items-center justify-around mx-auto">
         <Link href="/" className="flex items-center justify-center">
-            <span className="hover:text-2xl self-center text-xl font-semibold whitespace-nowrap dark:text-white">Home</span>
+          <span className="hover:text-2xl self-center text-xl font-semibold whitespace-nowrap dark:text-white">Home</span>
         </Link>
-        <Link href="/login" className="flex items-center justify-center">
-            <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Login</span>
-        </Link>
-        <Link href="/register" className="flex items-center justify-center">
-            <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Register</span>
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link href="/profile" className="flex items-center justify-center">
+              <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Profile</span>
+            </Link>
+            <Link href="/generate" className="flex items-center justify-center">
+              <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Generate</span>
+            </Link>
+            <button className="flex items-center justify-center" onClick={handleLogout}>
+              <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Logout</span>
+            </button>
+          </>
+        ) : (
+          <>
+            <Link href="/explore" className="flex items-center justify-center">
+              <span className="hover:text-2xl self-center text-xl font-semibold whitespace-nowrap dark:text-white">Explore</span>
+            </Link>
+            <Link href="/login" className="flex items-center justify-center">
+              <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Login</span>
+            </Link>
+            <Link href="/register" className="flex items-center justify-center">
+              <span className="hover:text-2xl text-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">Register</span>
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
--- journey-craft/pages/explore.tsx
+++ journey-craft/pages/explore.tsx
@@ -1,14 +1,16 @@
-// pages/index.tsx
+import Layout from '@/components/layout';
 
 import React from 'react';
 
 const HomePage: React.FC = () => {
   return (
+    <Layout>
     <div>
       <h1>Welcome to JourneyCraft</h1>
       <p>Experience personalized travel itineraries for the post-pandemic era.</p>
       <button>Get Started</button>
     </div>
+    </Layout>
   );
 };
 
